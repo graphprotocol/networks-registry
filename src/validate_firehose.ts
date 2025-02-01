747eee5375b57782c82052eeabacd626c87bc6eb
Yaro Shkvorets <shkvorets@gmail.com>
2025-02-01 11:18:35 -0500

    Add maintenance github action (#59) [no ci]
    * add maintenance script and gh action
    
    * adjust maintenance action
    
    * rename action file
    
    * install grpcurl

--- src/validate_firehose.ts
+++ src/validate_firehose.ts
@@ -1,48 +1,77 @@
+import { printErrorsAndWarnings } from "./print";
 import { Network } from "./types/registry";
 import { loadNetworks } from "./utils/fs";
-import { exec } from 'child_process';
-import { promisify } from 'util';
+import { exec } from "child_process";
+import { promisify } from "util";
 
 const execAsync = promisify(exec);
 
 const ERRORS: string[] = [];
+const WARNINGS: string[] = [];
 
 interface FirehoseInfo {
   chainName: string;
   chainNameAliases: string[];
   firstStreamableBlockNum?: string;
   firstStreamableBlockId: string;
-  blockIdEncoding: "BLOCK_ID_ENCODING_HEX" | "BLOCK_ID_ENCODING_BASE58" | "BLOCK_ID_ENCODING_0X_HEX";
+  blockIdEncoding:
+    | "BLOCK_ID_ENCODING_HEX"
+    | "BLOCK_ID_ENCODING_BASE58"
+    | "BLOCK_ID_ENCODING_0X_HEX";
   blockFeatures: ("base" | "extended" | "hybrid")[];
 }
 
 const ValidEncodingMap = {
-  "BLOCK_ID_ENCODING_HEX": "hex",
-  "BLOCK_ID_ENCODING_BASE58": "base58",
-  "BLOCK_ID_ENCODING_0X_HEX": "0xhex",
+  BLOCK_ID_ENCODING_HEX: "hex",
+  BLOCK_ID_ENCODING_BASE58: "base58",
+  BLOCK_ID_ENCODING_0X_HEX: "0xhex",
 };
 
-async function validateSingleEndpoint(network: Network, endpoint: string): Promise<void> {
+async function validateSingleEndpoint(
+  network: Network,
+  endpoint: string,
+): Promise<void> {
   console.log(`  ${network.id} @ ${endpoint} `);
   try {
     const command = `grpcurl -H "X-Api-Key: ${process.env.SF_API_KEY}" ${endpoint} sf.firehose.v2.EndpointInfo/Info`;
     const { stdout } = await execAsync(command);
     const info = JSON.parse(stdout) as FirehoseInfo;
 
-    if (ValidEncodingMap[info.blockIdEncoding] !== network.firehose?.bytesEncoding) {
-      ERRORS.push(`Network ${network.id} endpoint ${endpoint} has invalid bytesEncoding: Registry: ${network.firehose?.bytesEncoding}, Firehose: ${info.blockIdEncoding}`);
+    if (
+      ValidEncodingMap[info.blockIdEncoding] !== network.firehose?.bytesEncoding
+    ) {
+      ERRORS.push(
+        `Network ${network.id} endpoint ${endpoint} has invalid bytesEncoding: Registry: ${network.firehose?.bytesEncoding}, Firehose: ${info.blockIdEncoding}`,
+      );
     }
-    if (info.blockFeatures?.includes("extended") && !network.firehose?.evmExtendedModel) {
-      ERRORS.push(`Network ${network.id} endpoint ${endpoint} has invalid evmExtendedModel for extended: Registry: ${network.firehose?.evmExtendedModel}, Firehose: ${info.blockFeatures}`);
+    if (
+      info.blockFeatures?.includes("extended") &&
+      !network.firehose?.evmExtendedModel
+    ) {
+      ERRORS.push(
+        `Network ${network.id} endpoint ${endpoint} has invalid evmExtendedModel for extended: Registry: ${network.firehose?.evmExtendedModel}, Firehose: ${info.blockFeatures}`,
+      );
     }
-    if (info.blockFeatures?.includes("base") && network.firehose?.evmExtendedModel) {
-      ERRORS.push(`Network ${network.id} endpoint ${endpoint} has invalid evmExtendedModel for base: Registry: ${network.firehose?.evmExtendedModel}, Firehose: ${info.blockFeatures}`);
+    if (
+      info.blockFeatures?.includes("base") &&
+      network.firehose?.evmExtendedModel
+    ) {
+      ERRORS.push(
+        `Network ${network.id} endpoint ${endpoint} has invalid evmExtendedModel for base: Registry: ${network.firehose?.evmExtendedModel}, Firehose: ${info.blockFeatures}`,
+      );
     }
-    if (info.blockFeatures?.includes("hybrid") && !network.firehose?.evmExtendedModel) {
-      ERRORS.push(`Network ${network.id} endpoint ${endpoint} has invalid evmExtendedModel for hybrid: Registry: ${network.firehose?.evmExtendedModel}, Firehose: ${info.blockFeatures}`);
+    if (
+      info.blockFeatures?.includes("hybrid") &&
+      !network.firehose?.evmExtendedModel
+    ) {
+      ERRORS.push(
+        `Network ${network.id} endpoint ${endpoint} has invalid evmExtendedModel for hybrid: Registry: ${network.firehose?.evmExtendedModel}, Firehose: ${info.blockFeatures}`,
+      );
     }
     if (!network.genesis?.hash?.includes(info.firstStreamableBlockId)) {
-      ERRORS.push(`Network ${network.id} endpoint ${endpoint} has invalid firstStreamableBlockId: Registry: ${network.genesis?.hash}, Firehose: ${info.firstStreamableBlockId}`);
+      ERRORS.push(
+        `Network ${network.id} endpoint ${endpoint} has invalid firstStreamableBlockId: Registry: ${network.genesis?.hash}, Firehose: ${info.firstStreamableBlockId}`,
+      );
     }
   } catch (error) {
     if (error instanceof Error) {
@@ -50,7 +79,9 @@ async function validateSingleEndpoint(network: Network, endpoint: string): Promi
         return;
       }
     }
-    ERRORS.push(`Network ${network.id} endpoint ${endpoint} error: ${JSON.stringify(error)}`);
+    ERRORS.push(
+      `Network ${network.id} endpoint ${endpoint} error: ${JSON.stringify(error)}`,
+    );
   }
 }
 
@@ -61,27 +92,40 @@ async function validateFirehoseEvmModel(networks: Network[]) {
   }
   console.log("Validating firehose evm extended model ");
 
-  const endpointsAndNetworks = networks
-    .flatMap(network => network.services.firehose?.map(endpoint => [endpoint, network] as [string, Network]) ?? []);
+  const endpointsAndNetworks = networks.flatMap(
+    (network) =>
+      network.services.firehose?.map(
+        (endpoint) => [endpoint, network] as [string, Network],
+      ) ?? [],
+  );
 
   const BATCH = 20;
   for (let i = 0; i < endpointsAndNetworks.length; i += BATCH) {
     const chunk = endpointsAndNetworks.slice(i, i + BATCH);
-    await Promise.all(chunk.map(([endpoint, network]) => validateSingleEndpoint(network, endpoint)));
+    await Promise.all(
+      chunk.map(([endpoint, network]) =>
+        validateSingleEndpoint(network, endpoint),
+      ),
+    );
   }
 }
 
-async function main() {
-  const [, , networksPath = "registry"] = process.argv;
-
+export async function validateFirehose(networksPath: string) {
   let networks = loadNetworks(networksPath);
   console.log(`Loaded ${networks.length} networks`);
 
   await validateFirehoseEvmModel(networks);
 
-  if (ERRORS.length > 0) {
-    console.error("\nValidation errors found:");
-    ERRORS.forEach((error) => console.error(`  ${error}`));
+  return { errors: ERRORS, warnings: WARNINGS };
+}
+
+async function main() {
+  const [, , networksPath = "registry"] = process.argv;
+
+  const { errors, warnings } = await validateFirehose(networksPath);
+
+  printErrorsAndWarnings(errors, warnings);
+  if (errors.length > 0) {
     process.exit(1);
   }
 }
