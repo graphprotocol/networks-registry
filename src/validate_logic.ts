f8f649c7b5c3e28e1c8ad32cf28f7fc6a7d58440
AK <alikim@gmail.com>
2025-10-06 12:21:10 -0400

    Zksyncos preview (#168)
    * added zksync os preview
    
    * backport
    
    * chore: auto-bump version to 0.7.37 (patch)
    
    * name update
    
    * backport
    
    * update name : devnet
    
    ---------
    
    Co-authored-by: Pinax Bot <ops@pinax.network>

--- src/validate_logic.ts
+++ src/validate_logic.ts
@@ -20,6 +20,7 @@ const ALLOWED_ETHEREUM_LIST_MISSING: string[] = [
   "autonomys",
   "autonomys-chronos",
   "sonic-testnet",
+  "zksync-os-devnet",
 ];
 
 function validateFilenames(networksPath: string) {
