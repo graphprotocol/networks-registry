4ddd6b8254437c5b82e727578a4daa2ec084fade
Ali Kim <alikim@gmail.com>
2025-10-06 10:26:52 -0400

    added zksync os preview

--- src/validate_logic.ts
+++ src/validate_logic.ts
@@ -20,6 +20,7 @@ const ALLOWED_ETHEREUM_LIST_MISSING: string[] = [
   "autonomys",
   "autonomys-chronos",
   "sonic-testnet",
+  "zksync-os-preview",
 ];
 
 function validateFilenames(networksPath: string) {
